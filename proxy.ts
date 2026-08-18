5b3444054fe4715c936d4e42255ceefc2c5c7349
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2026-08-18 23:41:02 +0500

    feat: setup auth and proxy for gating

---
+++ proxy.ts
@@ -0,0 +1,21 @@
+import { clerkMiddleware } from '@clerk/nextjs/server'
+
+export default clerkMiddleware(async (auth, req) => {
+  const pathname = req.nextUrl.pathname
+  if (pathname === '/dashboard' || pathname.startsWith('/dashboard/')) {
+    const { isAuthenticated, redirectToSignIn } = await auth()
+
+    if (!isAuthenticated) return redirectToSignIn()
+  }
+})
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+    // Always run for Clerk-specific frontend API routes
+    '/__clerk/(.*)',
+  ],
+}
